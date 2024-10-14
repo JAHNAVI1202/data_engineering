7761c9071c377d49a3d68f1fb750bc00805f961f
Jahnavi <jhansi3198@gmail.com>
2024-10-14 09:15:26 -0400

    saved changs

--- Python/guess_number.py
+++ Python/guess_number.py
@@ -5,9 +5,9 @@ print(guess_number)
 chances =3
 i=1
 while(i<=chances):
-    a=int(input("enter your Guess") )
+    a=int(input("enter your Guess: ") )
     if guess_number == a:
-        i+=1
+       # i+=1
         print("you guessed it right")
         break
     else:
